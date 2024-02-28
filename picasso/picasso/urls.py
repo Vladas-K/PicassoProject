4184e8b149d24720ad62d0691b4180f2e5bd9a0e
Владас Куодис <vlad250577@yandex.ru>
2024-02-28 17:29:50 +0300

    Запуск основного функцинала

--- picasso/picasso/urls.py
+++ picasso/picasso/urls.py
@@ -1,21 +1,17 @@
-"""picasso URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from fileflow.views import FileViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('files/', FileViewSet.as_view({'get': 'list'})),
+    path('upload/', FileViewSet.as_view({'post': 'create'})),  
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
